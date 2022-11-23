1a3ed8d82fbb1d71c45921db11cd4cfa8801fea1
vladdd1792 <107995468+vladdd1792@users.noreply.github.com>
2022-11-23 13:58:48 +0200

    Add files via upload

---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryLst = createGalleryCard(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryLst);
+
+function createGalleryCard(cardItems) {
+    return cardItems
+      .map(({ preview, original, description }) => {
+        return `
+          <a class="gallery__item"
+          href="${original}">
+          <img class="gallery__image"
+              src="${preview}" 
+              alt=${description}" />
+          </a>
+          `;
+      })
+      .join("");
+  }
+  
+
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 300,
+  });
+  
